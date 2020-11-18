e60b608131bed81c3897a972c1394df6f3460392
Volodymyr77 <gagarinvov@gmail.com>
2020-11-19 00:01:22 +0200

    usestate

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
+import {useMemo, useState} from "react";
+
+const Test = () => {
+  console.log('test resf');
+  return<h1> Hello from test</h1>
+}
 
 function App() {
+
+
+
+  // const [someArr, setSomeArr] = useState([]);
+  // const [cycles1, setCycles1] = useState(20);
+  // const [cycles2, setCycles2] = useState(20);
+  // const [cycles3, setCycles3] = useState(20);
+
+  const calculateHardStuff = (cyclesCount)  => {
+    let counter = 0;
+    for (let i=0; i<cyclesCount; i++) {
+      counter++;
+      console.log(`i was called $[{i}`)
+    }
+    return counter;
+  }
+
+  const  someComplicatedValue = useMemo(( ) =>
+      calculateHardStuff(cycles1, cycles2, cycles3), [cycles1])
+  // const  someComplicatedValue = calculateHardStuff(5);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className="App"
+      onClick={() => {
+      setSomeArr([...someArr, 1]);
+      }}
+    >
+      hello world hard calc value {someComplicatedValue}
+      {someArr.map((el) => {
+        <h3 key={el}>{el}</h3>
+      })}
     </div>
+
   );
+
 }
 
 export default App;
